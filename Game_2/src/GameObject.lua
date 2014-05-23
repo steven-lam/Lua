5d7057318bfcb1a0c2afce9b938d6cd11da4b9f0
Steven <lam.steven94@yahoo.com>
2014-05-22 17:15:13 -0700

    Added classes and base class GameObject

---
+++ Game_2/src/GameObject.lua
@@ -0,0 +1,31 @@
+class = require "external/30log/30log"
+
+GameObject = class()
+
+function GameObject:__init()
+	self.x = 0
+	self.y = 0
+	self.w = 0
+	self.h = 0
+end
+
+
+function GameObject:update(dt) 
+end
+
+function GameObject:getX()
+	return self.body:getX()
+end
+
+function GameObject:getY()
+	return self.body:getY()
+end
+
+function GameObject:getWidth()
+	return self.w
+end
+
+function GameObject:getHeight()
+	return self.h
+end
+
