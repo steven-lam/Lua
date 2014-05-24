3f1149c016f3af82d9185d6924f2be63fe177a49
Steven <lam.steven94@yahoo.com>
2014-05-23 17:58:41 -0700

    Changed Patterns to matrices starting at 1

--- Game_2/src/Pattern.lua
+++ Game_2/src/Pattern.lua
@@ -1,47 +1,104 @@
 
 
-function Pattern(randomPattern) 
+function Pattern(randomPattern, horzPattern) 
 	-- space difference between two carrots
-	size = 50
-	-- table to keep track of carrot spawn values
-	y = {}	
-	instaSpawn = false
-	--			x
-	--		x   	x
-	--	x 				x
-	
-	if(randomPattern == 0) then
-		y[0] = math.random() * 450 + size * 2
-		y[1] = y[0]  - size
-		y[2] = y[0]  - size * 2
-		y[3] = y[0]  - size
-		y[4] = y[0] 
+	local size = 50
+
+	-- table size
+	local tableSize = 5
+
+	-- if current pattern is vertical or not
+	local vertical = false
+
+	-- value to keep track of starting y-axis value
+	local y = 0
+
+	-- matrix to keep track of carrot spawn values
+	matrix = {}
+	for i=1,tableSize do
+		mt[i] = {}
+		for j=1,tableSize do
+			matrix[i][j] = false
+		end
+	end
+	---------------------------------------------
+	-- if pattern requested is horizontal
+	if(horzPattern) then
+		--			x
+		--		x   	x
+		--	x 				x
+		
+		if(randomPattern == 0) then
+			y = math.random() * 450 + size * 2 
+			matrix[1][3] = true
+			matrix[2][2] = true
+			matrix[3][1] = true
+			matrix[4][2] = true
+			matrix[5][3] = true
+		end
+
+		-- x x x x x
+
+		if(randomPattern == 1) then
+			y = math.random() * 500 + size
+			for i=1,tableSize do
+				matrix[0][i] = true 
+			end
+		end
+
+		-- x x x x x
+		-- x x x x x
+
+		if(randomPattern = 2) then
+			y = math.random() * 500 + size
+			for i=1,2 do
+				for j=1, tableSize do
+					matrix[i][j] = true
+				end
+			end
+		end
+	else
+		vertical = true
+		-- x
+		-- x
+		-- x
+		-- x
+		-- x
+
+		if(randomPattern == 0) then
+			y = math.random() * 250 + size
+			for i=1,tableSize do
+				matrix[i][0] = true
+			end
+		end
+
+		-- x x
+		-- x x
+		-- x x
+		-- x x
+		-- x x 
+
+		if(randomPattern == 1) then
+			y = math.random() * 250 + size
+			for i=1,2 do
+				for j=1,tableSize do
+					matrix[j][i] = true
+				end
+			end
+		end
+
+		if(randomPattern == 2) then
+			y = math.random() * 250 + size
+			matrix[3][1] = true
+			matrix[2][2] = true
+			matrux[4][2] = true
+			matrix[1][3] = true
+			matrix[5][3] = true
+			matrix[2][4] = true
+			matrix[4][4] = true
+			matrix[3][5] = true
+		end
 	end
 
-	-- x x x x x
-
-	if(randomPattern == 1) then
-		y[0] = math.random() * 500 + size
-		y[1] = y[0]
-		y[2] = y[0]
-		y[3] = y[0]
-		y[4] = y[0]
-	end
-
-	--x
-	--x
-	--x
-	--x
-	--x
-
-	if(randomPattern == 2) then
-		y[0]= math.random() * 250 + size
-		y[1] = y[0] + size
-		y[2] = y[1] + size
-		y[3] = y[2] + size
-		y[4] = y[3] + size
-		instaSpawn = true
-	end
-
-	return y, instaSpawn
+	return matrix, y, vertical
 end
