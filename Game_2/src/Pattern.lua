f76e79095306df81dd1f59b0aa9a8b3fd96a6be1
Steven <lam.steven94@yahoo.com>
2014-05-23 18:00:49 -0700

    Changed pattern back to starting at 0:

--- Game_2/src/Pattern.lua
+++ Game_2/src/Pattern.lua
@@ -5,7 +5,7 @@ function Pattern(randomPattern, horzPattern)
 	local size = 50
 
 	-- table size
-	local tableSize = 5
+	local tableSize = 4
 
 	-- if current pattern is vertical or not
 	local vertical = false
@@ -15,9 +15,9 @@ function Pattern(randomPattern, horzPattern)
 
 	-- matrix to keep track of carrot spawn values
 	matrix = {}
-	for i=1,tableSize do
+	for i=0,tableSize do
 		mt[i] = {}
-		for j=1,tableSize do
+		for j=0,tableSize do
 			matrix[i][j] = false
 		end
 	end
@@ -30,19 +30,19 @@ function Pattern(randomPattern, horzPattern)
 		
 		if(randomPattern == 0) then
 			y = math.random() * 450 + size * 2 
-			matrix[1][3] = true
-			matrix[2][2] = true
+			matrix[0][2] = true
+			matrix[1][1] = true
+			matrix[2][0] = true
 			matrix[3][1] = true
 			matrix[4][2] = true
-			matrix[5][3] = true
 		end
 
 		-- x x x x x
 
 		if(randomPattern == 1) then
 			y = math.random() * 500 + size
-			for i=1,tableSize do
-				matrix[0][i] = true 
+			for i=0,tableSize do
+				matrix[0][1] = true 
 			end
 		end
 
@@ -51,13 +51,14 @@ function Pattern(randomPattern, horzPattern)
 
 		if(randomPattern = 2) then
 			y = math.random() * 500 + size
-			for i=1,2 do
-				for j=1, tableSize do
+			for i=0,1 do
+				for j=0, tableSize do
 					matrix[i][j] = true
 				end
 			end
 		end
 	else
+		
 		vertical = true
 		-- x
 		-- x
@@ -67,7 +68,7 @@ function Pattern(randomPattern, horzPattern)
 
 		if(randomPattern == 0) then
 			y = math.random() * 250 + size
-			for i=1,tableSize do
+			for i=0,tableSize do
 				matrix[i][0] = true
 			end
 		end
@@ -80,8 +81,8 @@ function Pattern(randomPattern, horzPattern)
 
 		if(randomPattern == 1) then
 			y = math.random() * 250 + size
-			for i=1,2 do
-				for j=1,tableSize do
+			for i=0,1 do
+				for j=0,tableSize do
 					matrix[j][i] = true
 				end
 			end
@@ -89,14 +90,14 @@ function Pattern(randomPattern, horzPattern)
 
 		if(randomPattern == 2) then
 			y = math.random() * 250 + size
-			matrix[3][1] = true
-			matrix[2][2] = true
-			matrux[4][2] = true
+			matrix[2][0] = true
+			matrix[1][1] = true
+			matrux[3][1] = true
+			matrix[0][2] = true
+			matrix[4][2] = true
 			matrix[1][3] = true
-			matrix[5][3] = true
+			matrix[3][3] = true
 			matrix[2][4] = true
-			matrix[4][4] = true
-			matrix[3][5] = true
 		end
 	end
 
